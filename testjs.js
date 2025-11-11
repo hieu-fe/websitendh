4868390b7ae24a21d10955efa8a4662047b26814
hieu-fe <nguyenduchieu100404@gmail.com>
2025-11-11 11:11:49 +0700

    First commit

---
+++ testjs.js
@@ -0,0 +1,14 @@
+function x() {
+    document.getElementById("demo").innerHTML = ten + "_" + tuoi +"_ va nam sinh la la:"+ h;
+}
+function a(){
+    window.alert("hien thi alert")
+    document.getElementById("demo2").innerHTML=tuoi;
+    console.log("hien thi console")
+}
+var ten = "hieu";
+let tuoi = 20+10;
+const h = 1000;
+tuoi++;
+console.log("Hello " + ten + tuoi + h);
+        
